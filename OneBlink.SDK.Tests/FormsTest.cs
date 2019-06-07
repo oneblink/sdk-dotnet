1ba79b7214a4697a109e03dd131d418a44428b30
Aaron Roworth <aaron@blinkmobile.com.au>
2019-06-07 10:11:00 +1000

    merge conflicts

--- OneBlink.SDK.Tests/FormsTest.cs
+++ OneBlink.SDK.Tests/FormsTest.cs
@@ -4,40 +4,46 @@ using OneBlink.SDK;
 
 namespace unit_tests
 {
-    public class FormsTest
+  public class FormsTest
+  {
+    private string ACCESS_KEY = Environment.GetEnvironmentVariable("ACCESS_KEY");
+    private string SECRET_KEY = Environment.GetEnvironmentVariable("SECRET_KEY");
+    [Fact]
+    public void can_be_constructed()
     {
-        [Fact]
-        public void can_be_constructed()
-        {
-            Forms forms = new Forms("a", "b");
-            Assert.NotNull(forms);
-        }
-
-        [Fact]
-        public void throws_error_if_keys_empty() {
-            try
-            {
-                Forms forms = new Forms("", "");
-            }
-            catch (Exception ex) {
-                Assert.NotNull(ex);
-            }
-        }
-
-        [Fact]
-        public async void can_search_forms() {
-            Forms forms = new Forms("a", "b"); // TODO Get creds from config
-            string response = await forms.search(null, null, null);
-            Assert.NotNull(response);
-            // Console.WriteLine("Response: " + response);
-        }
-
-        [Fact]
-        public async void can_search_forms_with_all_params() {
-            Forms forms = new Forms("a", "b"); // TODO Get creds from config
-            string response = await forms.search(true, true, "Location test");
-            Assert.NotNull(response);
-            // Console.WriteLine("Response: " + response);
-        }
+      Forms forms = new Forms(ACCESS_KEY, SECRET_KEY);
+      Assert.NotNull(forms);
     }
+
+    [Fact]
+    public void throws_error_if_keys_empty()
+    {
+      try
+      {
+        Forms forms = new Forms(ACCESS_KEY, SECRET_KEY);
+      }
+      catch (Exception ex)
+      {
+        Assert.NotNull(ex);
+      }
+    }
+
+    [Fact]
+    public async void can_search_forms()
+    {
+      Forms forms = new Forms(ACCESS_KEY, SECRET_KEY); // TODO Get creds from config
+      string response = await forms.search(null, null, null);
+      Assert.NotNull(response);
+      // Console.WriteLine("Response: " + response);
+    }
+
+    [Fact]
+    public async void can_search_forms_with_all_params()
+    {
+      Forms forms = new Forms(ACCESS_KEY, SECRET_KEY); // TODO Get creds from config
+      string response = await forms.search(true, true, "Location test");
+      Assert.NotNull(response);
+      // Console.WriteLine("Response: " + response);
+    }
+  }
 }
