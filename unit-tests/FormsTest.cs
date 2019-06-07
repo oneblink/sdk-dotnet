f547786b7be9333fcdd185e02092ed7e13793daa
Aaron Roworth <aaron@blinkmobile.com.au>
2019-06-07 10:06:02 +1000

    rebase

--- unit-tests/FormsTest.cs
+++ unit-tests/FormsTest.cs
@@ -13,24 +13,38 @@ namespace unit_tests
     public void can_be_constructed()
     {
       Forms forms = new Forms("a", "b");
+      Assert.NotNull(forms);
     }
 
     [Fact]
-    public void can_search_forms()
+    public void throws_error_if_keys_empty()
+    {
+      try
+      {
+        Forms forms = new Forms(ACCESS_KEY, SECRET_KEY);
+      }
+      catch (Exception ex)
+      {
+        Assert.NotNull(ex);
+      }
+    }
+
+    [Fact]
+    public async void can_search_forms()
     {
       Forms forms = new Forms(ACCESS_KEY, SECRET_KEY); // TODO Get creds from config
-      string response = forms.search(null, null, string.Empty);
-      Assert.True(response != null);
+      string response = await forms.search(null, null, null);
+      Assert.NotNull(response);
       // Console.WriteLine("Response: " + response);
     }
 
     [Fact]
-    public void can_search_forms_with_all_params()
+    public async void can_search_forms_with_all_params()
     {
       Forms forms = new Forms(ACCESS_KEY, SECRET_KEY); // TODO Get creds from config
-      string response = forms.search(true, true, "Location test");
-      Assert.True(response != null);
-      Console.WriteLine("Response: " + response);
+      string response = await forms.search(true, true, "Location test");
+      Assert.NotNull(response);
+      // Console.WriteLine("Response: " + response);
     }
   }
 }
