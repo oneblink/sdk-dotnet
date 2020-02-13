338fdadee4675438e04fdb2e1280d0625dac93a2
Zac Turner <zac@oneblink.io>
2020-02-13 15:21:25 +1100

    ON-4986 # Changed csproj file to grant access to internal classes to OneBlink.SDK.Tests. Enabled region testing

--- OneBlink.SDK.Tests/RegionTests.cs
+++ OneBlink.SDK.Tests/RegionTests.cs
@@ -6,39 +6,39 @@ namespace OneBlink.SDK.Tests
 {
     public class RegionTests {
 
-        // [Fact]
-        // public void can_get_region_urls()
-        // {
-        //     Region region = new Region(RegionCode.US);
-        //     Assert.Matches("https://us-auth-api.blinkm.io", region.oneBlinkAPIOrigin);
-        //     Assert.Matches("https://us-pdf.blinkm.io", region.oneBlinkPdfOrigin);
+        [Fact]
+        public void can_get_region_urls()
+        {
+            Region region = new Region(RegionCode.US);
+            Assert.Matches("https://us-auth-api.blinkm.io", region.oneBlinkAPIOrigin);
+            Assert.Matches("https://us-pdf.blinkm.io", region.oneBlinkPdfOrigin);
 
-        //     region = new Region(RegionCode.AU);
-        //     Assert.Matches("https://auth-api.blinkm.io", region.oneBlinkAPIOrigin);
-        //     Assert.Matches("https://pdf.blinkm.io", region.oneBlinkPdfOrigin);
+            region = new Region(RegionCode.AU);
+            Assert.Matches("https://auth-api.blinkm.io", region.oneBlinkAPIOrigin);
+            Assert.Matches("https://pdf.blinkm.io", region.oneBlinkPdfOrigin);
 
-        // }
+        }
 
-        // [Fact]
-        // public void region_defaults_to_au()
-        // {
-        //     Region region = new Region();
-        //     Assert.Matches("https://auth-api.blinkm.io", region.oneBlinkAPIOrigin);
-        //     Assert.Matches("https://pdf.blinkm.io", region.oneBlinkPdfOrigin);
-        // }
+        [Fact]
+        public void region_defaults_to_au()
+        {
+            Region region = new Region();
+            Assert.Matches("https://auth-api.blinkm.io", region.oneBlinkAPIOrigin);
+            Assert.Matches("https://pdf.blinkm.io", region.oneBlinkPdfOrigin);
+        }
 
-        // [Fact]
-        // public void can_override_region_urls()
-        // {
-        //     Region region = new Region(
-        //         RegionCode.US,
-        //         apiOrigin: "https://auth-api-test.blinkm.io",
-        //         pdfOrigin: "https://pdf-test.blinkm.io"
-        //     );
-        //     Console.WriteLine(region.oneBlinkAPIOrigin);
-        //     Assert.Matches("https://auth-api-test.blinkm.io", region.oneBlinkAPIOrigin);
-        //     Assert.Matches("https://pdf-test.blinkm.io", region.oneBlinkPdfOrigin);
-        // }
+        [Fact]
+        public void can_override_region_urls()
+        {
+            Region region = new Region(
+                RegionCode.US,
+                apiOrigin: "https://auth-api-test.blinkm.io",
+                pdfOrigin: "https://pdf-test.blinkm.io"
+            );
+            Console.WriteLine(region.oneBlinkAPIOrigin);
+            Assert.Matches("https://auth-api-test.blinkm.io", region.oneBlinkAPIOrigin);
+            Assert.Matches("https://pdf-test.blinkm.io", region.oneBlinkPdfOrigin);
+        }
 
     }
 
