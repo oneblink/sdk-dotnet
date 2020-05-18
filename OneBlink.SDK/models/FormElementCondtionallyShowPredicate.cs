254d24fc615d6e29120a5cd10b3576e0354cb764
kizaonline <kizaonline@gmail.com>
2020-05-18 16:21:22 +1000

    ON-6187 # Add new predicate property

--- OneBlink.SDK/models/FormElementCondtionallyShowPredicate.cs
+++ OneBlink.SDK/models/FormElementCondtionallyShowPredicate.cs
@@ -1,14 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OneBlink.SDK.Model
 {
     public class FormElementConditionallyShowPredicate
     {
         public Guid elementId { get; set; }
-        public string type { get; set; }
+        private string[] AllowedTypes = new string[] { "OPTIONS", "NUMERIC", "VALUE" };
+        private string _Type;
+        public string type
+        {
+            get
+            {
+                return _Type;
+            }
+            set
+            {
+                if (!AllowedTypes.Any(x => x == value))
+                {
+                    throw new ArgumentException(value = " not a valid Form Element Conditionally Show Predicate Type");
+                }
+                _Type = value;
+            }
+        }
         public List<string> optionIds { get; set; }
         public string @operator { get; set; }
         public int value { get; set; }
+        public Boolean hasValue { get; set; }
     }
 }
