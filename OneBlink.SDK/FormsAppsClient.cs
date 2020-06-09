393a212f441a0543b3b1fa6cd14c3245a462a241
Zac Turner <zac@oneblink.io>
2020-06-10 09:44:15 +1000

    ON-6477 # Removed try catch

--- OneBlink.SDK/FormsAppsClient.cs
+++ OneBlink.SDK/FormsAppsClient.cs
@@ -22,20 +22,16 @@ namespace OneBlink.SDK
 
         public async Task<JWTPayload> VerifyJWT(string token)
         {
-            try {
-                if (token.Contains("Bearer ")) {
-                    token = token.Split(' ')[1];
-                }
-                JwtSecurityToken jwt = new JwtSecurityToken(token);
-                JsonWebKey jwk = await Token.GetJsonWebKey(
-                    iss: this.oneBlinkApiClient.tenant.jwtIssuer,
-                    kid: jwt.Header.Kid
-                );
-                string verifiedToken = Token.VerifyJWT(token, jwk);
-                return JsonConvert.DeserializeObject<JWTPayload>(verifiedToken);
-            } catch(Exception err) {
-                throw new Exception("The supplied JWT was invalid.");
+            if (token.Contains("Bearer ")) {
+                token = token.Split(' ')[1];
             }
+            JwtSecurityToken jwt = new JwtSecurityToken(token);
+            JsonWebKey jwk = await Token.GetJsonWebKey(
+                iss: this.oneBlinkApiClient.tenant.jwtIssuer,
+                kid: jwt.Header.Kid
+            );
+            string verifiedToken = Token.VerifyJWT(token, jwk);
+            return JsonConvert.DeserializeObject<JWTPayload>(verifiedToken);
         }
 
     }
