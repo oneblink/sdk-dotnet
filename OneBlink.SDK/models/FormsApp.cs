1cbddf0b2281a66b8f53e2052b7040f882723b53
Zac Turner <zac@oneblink.io>
2020-11-23 16:53:44 +1100

    ON-9194 # Made formsapp props internal set only

--- OneBlink.SDK/models/FormsApp.cs
+++ OneBlink.SDK/models/FormsApp.cs
@@ -1,4 +1,5 @@
 using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace OneBlink.SDK.Model
@@ -8,17 +9,23 @@ namespace OneBlink.SDK.Model
         public long id { get; set; }
         public string name { get; set; }
         public string slug { get; set; }
-        public string hostname { get; set; }
-        public DateTime createdAt { get; set; }
-        public DateTime updatedAt { get; set; }
+        [JsonProperty]
+        public string hostname { get; internal set; }
+        [JsonProperty]
+        public DateTime createdAt { get; internal set; }
+        [JsonProperty]
+        public DateTime updatedAt { get; internal set; }
         public string organisationId { get; set; }
         public long formsAppEnvironmentId { get; set; }
-        public string oAuthClientId { get; set; }
+        [JsonProperty]
+        public string oAuthClientId { get; internal set; }
         public List<long> formIds { get; set; }
-        public FormsAppStyles styles { get; set; }
+        [JsonProperty]
+        public FormsAppStyles styles { get; internal set; }
         public FormsAppPWASettings pwaSettings { get; set; }
         public FormsAppWelcomeEmail welcomeEmail { get; set; }
-        public bool hasSamlIdentityProvider { get; set; }
+        [JsonProperty]
+        public bool hasSamlIdentityProvider { get; internal set; }
         public List<string> notificationEmailAddresses { get; set; }
 
     }
