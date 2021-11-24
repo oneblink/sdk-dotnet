88f3bc91407da7099e540768f3644e4f38500361
josh215 <josh@oneblink.io>
2021-11-24 16:49:25 +1100

    ON-17081 # Added FreshdeskCreateTicket to the submission events

--- OneBlink.SDK/models/FormSubmissionEventConfiguration.cs
+++ OneBlink.SDK/models/FormSubmissionEventConfiguration.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 namespace OneBlink.SDK.Model
 {
-    public class FormSubmissionEventConfigration
+    public class FormSubmissionEventConfiguration
     {
         public string url
         {
@@ -154,6 +154,11 @@ namespace OneBlink.SDK.Model
             get; set;
         }
         public FormSubmissionEventEmailTemplate emailTemplate
+        {
+            get; set;
+        }
+
+        public FormSubmissionEventFreshdeskFieldMapping[] freshdeskMapping
         {
             get; set;
         }
---
+++ OneBlink.SDK/models/FormSubmissionEventFreshdeskFieldMapping.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace OneBlink.SDK.Model
+{
+    public class FormSubmissionEventFreshdeskFieldMapping
+    {
+        public string formElementId
+        {
+            get; set;
+        }
+        public dynamic value
+        {
+            get; set;
+        }
+    }
+}
