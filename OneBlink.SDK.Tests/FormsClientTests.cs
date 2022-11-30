48a5801eca1ae5a76b472fc966d832ee6fe1c9d1
kizaonline <kizaonline@gmail.com>
2022-11-30 15:39:38 +1100

    ON-27741 # Update submissionId

--- OneBlink.SDK.Tests/FormsClientTests.cs
+++ OneBlink.SDK.Tests/FormsClientTests.cs
@@ -14,7 +14,7 @@ namespace OneBlink.SDK.Tests
         private string ACCESS_KEY;
         private string SECRET_KEY;
         private long formId = 475;
-        private string submissionId = "5ab3d950-253a-4d22-8ae6-c9eae82f58ba";
+        private string submissionId = "9e947f75-b952-4c45-ab37-f4429ecef1ff";
         private long draftFormId = 475;
         private string draftDataId = "b2925fbd-f490-49d9-ba07-1c57b97dd120";
         private string organisationId = "5c58beb2ff59481100000002";
