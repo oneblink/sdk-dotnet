a2d6aef96093e5bfe75045aa1f468ab691341e93
kizaonline <kizaonline@gmail.com>
2024-06-03 09:51:29 +1000

    ON-40885 # Change groups to object

---
+++ OneBlink.SDK/models/FormsAppGroup.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace OneBlink.SDK.Model
+{
+    public class FormsAppGroup
+    {
+        public string name
+        {
+            get; set;
+        }
+        public List<string> notificationEmailAddresses
+        {
+            get; set;
+        }
+    }
+}
